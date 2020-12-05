bfff92a53446e339dc83a73cdeb2153d7f3f033b
Dan Maxim <dmaxim@mxinformatics.com>
2020-12-05 12:02:27 -0700

    fix filenames and scripts

--- docker/generate-local-image.sh
+++ docker/generate-local-image.sh
@@ -9,15 +9,15 @@ docker build -t mxinfo.veritas.dbtemp:latest .
 
 echo "Running Image"
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" -p 1934:1433 --name mxinfo.veritas.temp -d mxinfo.veritas.dbtemp:latest
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" -p 50001:1433 --name mxinfo.veritas.temp -d mxinfo.veritas.dbtemp:latest
 
 echo "Initializing Database"
 
-docker exec mxinfo.veritas.temp sh -c "/opt/scripts/initialize.sh '$APP_USER_PASSWORD'"
+docker exec mxinfo.veritas.temp sh -c "/opt/scripts/initialize.sh '$SA_PASSWORD' '$APP_USER_PASSWORD'"
 
 echo "Generating Final Image"
 
-docker commit mxinfo.veritas.temp dmaxim/mxinfo.veritas.database:latest
+docker commit mxinfo.veritas.temp dmaxim/mxinfo.veritas.database:v1.0
 
 echo "Tear down intermediate containers"
 
--- docker/scripts/initialize.sh
+++ docker/scripts/initialize.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-appPasswordArg=$1
+saPasswordArg=$1
+appPasswordArg=$2
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i ./initialize.sql -o output.txt
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $saPasswordArg -i ./initialize.sql -o output.txt
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i ./create-database.sql -o create-output.txt
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $saPasswordArg -i ./create-database.sql -o create-output.txt
 
 echo :setvar appPassword $appPasswordArg > initialize-users.sql
 
-cat intialize-users-template.sql >> initialize-users.sql
+cat initialize-users-template.sql >> initialize-users.sql
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i ./initialize-users.sql -o user-output.txt 
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $saPasswordArg -i ./initialize-users.sql -o user-output.txt 
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i ./initialize-data.sql -o data-output.txt
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $saPasswordArg -i ./initialize-data.sql -o data-output.txt
 
 
